5cfb58b825f8da8316cfa91d1d358119d26d8c0f
Iceyclizz <iceyclizz@gmail.com>
2023-01-10 19:23:45 +0100

    Ejemplo pokemon

---
+++ lib/model.dart
@@ -0,0 +1,14 @@
+import 'package:angel_raul_alec_pokedex/pokemon.dart';
+import 'package:angel_raul_alec_pokedex/type.dart';
+
+class Model{
+  static final Model _model = Model._internal();
+
+  factory Model() {
+    return _model;
+  }
+
+  Model._internal();
+
+  var pokedex = <Pokemon>[Pokemon(no: 1,name: 'Bulbasur', type1: TypeList.planta,type2: TypeList.veneno),];
+}
